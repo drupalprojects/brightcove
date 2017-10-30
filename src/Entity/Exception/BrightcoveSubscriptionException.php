37b77251e64b29e06304bf2eec472fb8550f9fe1
Tamás NAGY <tamas.nagy@pronovix.com>
2017-10-30 13:17:28 +0100

    Add BrightcoveSubscriptionException class #3034

---
+++ src/Entity/Exception/BrightcoveSubscriptionException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\brightcove\Entity\Exception;
+
+/**
+ * Brightcove Subscription exception.
+ *
+ * @package Drupal\brightcove\Entity\Exception
+ */
+class BrightcoveSubscriptionException extends \Exception {}
